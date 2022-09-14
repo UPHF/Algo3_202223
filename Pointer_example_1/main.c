2572769374a41c86b378f074dd291c957a797a45
martinezmatias <matias.sebastian.martinez@gmail.com>
2022-09-14 15:14:10 +0200

    examples with pointer

--- Pointer_example_1/main.c
+++ Pointer_example_1/main.c
@@ -13,6 +13,20 @@ int main(int argc, char **argv)
 	
 	int max = 1000;
 	printf("Step 3) Max %d\n ", max);
-	printf("Step 2) Address Max %d \n", &max);
+	printf("Step 3) Address Max %d \n", &max);
+	
+	int * p;
+	p = &max;
+	printf("\nStep 4) p = &max; \n");
+	printf("Step 4) Value P %d\n ", p);
+	printf("Step 4) Address P %d\n ", &p);
+	printf("Step 4) Value *P %d\n ", *p);
+	
+	
+	p = &compteur;
+	printf("\nStep 4) p = &compteur; \n");
+	printf("Step 4) Value P %d\n ", p);
+	printf("Step 4) Address P %d\n ", &p);
+	printf("Step 4) Value *P %d\n ", *p);
 	
 }
