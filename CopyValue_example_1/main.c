00497db6d580d630f85bd46a4fbffec5364b2005
martinezmatias <matias.sebastian.martinez@gmail.com>
2022-09-14 15:56:00 +0200

    example copy value

--- CopyValue_example_1/main.c
+++ CopyValue_example_1/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+void increment(int input){
+		printf("Inside Increment Input value: %d \n", input);
+		printf("Address of Input: %d \n", &input);
+		input = input + 1;
+		printf("Inside Increment Input value after increment: %d \n", input);
+		
+}
+
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	int compteur = 0;
+	compteur = compteur + 1;
+	printf("Compteur before calling Increment %d\n", compteur);
+	increment(compteur);
+	printf("Compteur after  calling Increment %d\n", compteur);
+	printf("Address of Compteur: %d\n", &compteur);
 	return 0;
 }
