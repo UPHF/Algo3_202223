c07bb6bbee639a58e74d904c2dd4a32576a4df19
martinezmatias <matias.sebastian.martinez@gmail.com>
2022-09-14 15:05:55 +0200

    examples with simple vars

--- Pointer_example_1/main.c
+++ Pointer_example_1/main.c
@@ -2,6 +2,17 @@
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
-	return 0;
+	int compteur = 0;
+	printf("Step 1) Compteur %d\n ", compteur);
+	printf("Step 1) Address Compteur %d \n", &compteur);
+	
+	compteur = compteur + 10;
+	
+	printf("Step 2) Compteur %d\n ", compteur);
+	printf("Step 2) Address Compteur %d \n", &compteur);
+	
+	int max = 1000;
+	printf("Step 3) Max %d\n ", max);
+	printf("Step 2) Address Max %d \n", &max);
+	
 }
